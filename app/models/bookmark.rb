bbe11cdf9fa6527a48229cf15e6b4c2aa56fc059
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-15 21:57:44 +0200

    scss commit

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,6 @@
 class Bookmark < ApplicationRecord
-  belongs_to :list
   belongs_to :movie
-
-  validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list }
+  belongs_to :list
+  validates :comment, presence: true, length: { minimum: 6 }, allow_blank: false
+  validates :movie_id, uniqueness: { scope: [:movie_id, :list_id] }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, presence: true, uniqueness: true
-  validates :overview, presence: true
+  has_many :lists, through: :bookmarks
+  validates :title, presence: true, uniqueness: true, allow_blank: false
+  validates :overview, presence: true, allow_blank: false
 end
