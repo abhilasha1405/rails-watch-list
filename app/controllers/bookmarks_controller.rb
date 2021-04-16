843abc3c3fc4de8e40acb0f1799e1706541b3f8a
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-16 16:00:57 +0200

    Bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,4 @@
-cclass BookmarksController < ApplicationController
+class BookmarksController < ApplicationController
   before_action :set_bookmark, only: :destroy
   before_action :set_list, only: [:new, :create]
 
